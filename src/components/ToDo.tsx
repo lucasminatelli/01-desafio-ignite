d2fa05d26e9086beda1a1760100375148ed4ff5a
Lucas Rezende <lucas.minatelli@gmail.com>
2022-08-11 20:00:41 -0300

    todo header

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,8 +2,30 @@ import styles from "./ToDo.module.css";
 
 export function ToDo() {
   return (
-  <div className={styles.cotent}>
-    ToDo
+    <div>
+      <div>
+        <form>
+          <input placeholder="Adicione uma nova tarefa"></input>
+          <button>Criar</button>
+        </form>
+      </div>
+
+      <div>
+        <header className={styles.header}>
+          <div>
+            <strong className={styles.tarefas_criadas}>Tarefas criadas</strong>
+            <span>0</span>
+          </div>
+          <div>
+            <strong className={styles.tarefas_concluidas}>Concluídas</strong>
+            <span>0</span>
+          </div>
+        </header>
+
+        <div>
+          content
+        </div>
+      </div>
     </div>
-    );
+  );
 }
